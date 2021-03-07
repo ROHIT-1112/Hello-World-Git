bf40470315231fd32608212c5cb48c756c0a02d6
ROHIT CHATURVEDI <80148163+ROHIT-1112@users.noreply.github.com>
2021-03-07 13:25:53 +0530

    Add files via upload
    This is a Tutorial for upload of coded file from PC to  GitHub .

---
+++ max_of_three_integer.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the 1st Number :- "))
+b = int(input("Enter the 2nd Number :- "))
+c = int(input("Enter the 3rd Number :- "))
+if a>b and a>c:
+    print("A is Greater than B and C .")
+if b>a and b>c :
+    print("B is greater than A and C .")
+if c>a and c>a:
+    print("C is greater than A and B .")
